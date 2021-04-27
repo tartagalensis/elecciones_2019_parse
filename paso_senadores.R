a44273b0139343999583471ab850c9e6c4a3bbab
Franco Galeano <fgaleano1996@gmail.com>
2021-04-26 23:51:35 -0300

    update nqn, sgo, tdf, salta

--- paso_senadores.R
+++ paso_senadores.R
@@ -269,12 +269,375 @@ sen_er %>%
 
 sen_er %>% write_csv("data/definitivos/erios_sen_paso2019.csv")
 
+# NEUQUEN ####
+# -> 	CAT 000215000000000
 
-# NEUQUEN -> 	CAT 000215000000000
+votos_agrupaciones_nqn <- agrupaciones %>% 
+  filter(categoria == "000215000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria, agrupacion) %>% 
+  distinct(distrito, seccion, circuito, mesa, categoria, agrupacion,.keep_all = TRUE) %>% 
+  ungroup() %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria) %>% 
+  spread(agrupacion, votos) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  ungroup() %>% 
+  select(-categoria) %>% 
+  print()
+
+# Blancos Nulos
+votos_blancos_nulos_nqn <- blancos_nulos %>% 
+  filter(categoria == "000215000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  spread(contador, votos) %>% 
+  rename("blancos" = `VOTOS EN BLANCO`,
+         "nulos" = `VOTOS NULOS`,
+         "codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  select(-categoria) %>% 
+  ungroup() %>% 
+  print()
+
+# Electores
+electores_nqn <- electores_gral %>% 
+  filter(categoria == "000215000000000") %>%  
+  select(-contador, -categoria) %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  ungroup() %>% 
+  print()
+
+descripcion_postulaciones %>% 
+  filter(CODIGO_CATEGORIA == "000215000000000") %>% 
+  select(CODIGO_AGRUPACION, NOMBRE_AGRUPACION)
+
+read_csv("data/listas_sen_paso2019 copy.csv") %>% 
+  filter(vot_proCodigoProvincia == "15")
+
+# Join
+sen_nqn <- electores_nqn %>% 
+  left_join(secciones) %>% 
+  left_join(votos_blancos_nulos_nqn) %>% 
+  left_join(votos_agrupaciones_nqn) %>% 
+  rename( "00001" = `15-504`,
+          "00008" = `15-502`,
+          "00005" = `15-503`,
+          "00009" = `15-501`,
+          "00002" = `13`,
+          "00085" = `15-151`,
+          "electores" = "votos") %>% 
+  select(codprov, coddepto, depto, circuito, mesa, everything()) %>% 
+  print()
+
+sen_nqn %>%  
+  select(-circuito, -mesa) %>% 
+  summarise(across(electores:`00001`, sum))
+
+sen_nqn %>% write_csv("data/definitivos/neuquen_sen_paso2019.csv")
+
+# RIO NEGRO ####
+#-> 	CAT 000216000000000
 
 
-# RIO NEGRO -> 	CAT 000216000000000
-# SALTA -> CAT 000217000000000
-# SANTIAGO -> CAT 000222000000000
-# TDF -> 000224000000000
+votos_agrupaciones_rn <- agrupaciones %>% 
+  filter(categoria == "000216000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria, agrupacion) %>% 
+  distinct(distrito, seccion, circuito, mesa, categoria, agrupacion,.keep_all = TRUE) %>% 
+  ungroup() %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria) %>% 
+  spread(agrupacion, votos) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  ungroup() %>% 
+  select(-categoria) %>% 
+  print()
+
+# Blancos Nulos
+votos_blancos_nulos_rn <- blancos_nulos %>% 
+  filter(categoria == "000216000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  spread(contador, votos) %>% 
+  rename("blancos" = `VOTOS EN BLANCO`,
+         "nulos" = `VOTOS NULOS`,
+         "codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  select(-categoria) %>% 
+  ungroup() %>% 
+  print()
+
+# Electores
+electores_rn <- electores_gral %>% 
+  filter(categoria == "000216000000000") %>%  
+  select(-contador, -categoria) %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  ungroup() %>% 
+  print()
+
+descripcion_postulaciones %>% 
+  filter(CODIGO_CATEGORIA == "000216000000000") %>% 
+  select(CODIGO_AGRUPACION, NOMBRE_AGRUPACION)
+
+read_csv("data/listas_sen_paso2019 copy.csv") %>% 
+  filter(vot_proCodigoProvincia == "16")
+
+# Join
+sen_rn <- electores_rn %>% 
+  left_join(secciones) %>% 
+  left_join(votos_blancos_nulos_rn) %>% 
+  left_join(votos_agrupaciones_rn) %>% 
+  rename( "00008" = `16-503`,
+          "00005" = `16-502`,
+          "00086" = `16-169`,
+          "00002" = `13`,
+          "electores" = "votos") %>% 
+  select(codprov, coddepto, depto, circuito, mesa, everything()) %>% 
+  print()
+
+sen_rn %>%  
+  select(-circuito, -mesa) %>% 
+  summarise(across(electores:`00008`, sum))
+
+sen_rn %>% write_csv("data/definitivos/rnegro_sen_paso2019.csv")
+
+# SALTA ####
+# -> CAT 000217000000000
+
+
+votos_agrupaciones_salta <- agrupaciones %>% 
+  filter(categoria == "000217000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria, agrupacion) %>% 
+  distinct(distrito, seccion, circuito, mesa, categoria, agrupacion,.keep_all = TRUE) %>% 
+  ungroup() %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria) %>% 
+  spread(agrupacion, votos) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  ungroup() %>% 
+  select(-categoria) %>% 
+  print()
+
+# Blancos Nulos
+votos_blancos_nulos_salta <- blancos_nulos %>% 
+  filter(categoria == "000217000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  spread(contador, votos) %>% 
+  rename("blancos" = `VOTOS EN BLANCO`,
+         "nulos" = `VOTOS NULOS`,
+         "codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  select(-categoria) %>% 
+  ungroup() %>% 
+  print()
+
+# Electores
+electores_salta <- electores_gral %>% 
+  filter(categoria == "000217000000000") %>%  
+  select(-contador, -categoria) %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  ungroup() %>% 
+  print()
+
+descripcion_postulaciones %>% 
+  filter(CODIGO_CATEGORIA == "000217000000000") %>% 
+  select(CODIGO_AGRUPACION, NOMBRE_AGRUPACION)
+
+read_csv("data/listas_sen_paso2019 copy.csv") %>% 
+  filter(vot_proCodigoProvincia == "17")
+
+# Join
+sen_salta <- electores_salta %>% 
+  left_join(secciones) %>% 
+  left_join(votos_blancos_nulos_salta) %>% 
+  left_join(votos_agrupaciones_salta) %>% 
+  rename( "00088" = `19`,
+          "00008" = `17-504`,
+          "00005" = `17-501`,
+          "00009" = `17-503`,
+          "00087" = `17-502`,
+          "electores" = "votos") %>% 
+  select(codprov, coddepto, depto, circuito, mesa, everything()) %>% 
+  drop_na() %>% 
+  print()
+
+sen_salta %>%  
+  select(-circuito, -mesa) %>% 
+  summarise(across(electores:`00088`, sum))
+
+sen_salta %>% write_csv("data/definitivos/salta_sen_paso2019.csv")
+
+# SANTIAGO ####
+#-> CAT 000222000000000
+
+
+
+votos_agrupaciones_sgo <- agrupaciones %>% 
+  filter(categoria == "000222000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria, agrupacion) %>% 
+  distinct(distrito, seccion, circuito, mesa, categoria, agrupacion,.keep_all = TRUE) %>% 
+  ungroup() %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria) %>% 
+  spread(agrupacion, votos) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  ungroup() %>% 
+  select(-categoria) %>% 
+  print()
+
+# Blancos Nulos
+votos_blancos_nulos_sgo <- blancos_nulos %>% 
+  filter(categoria == "000222000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  spread(contador, votos) %>% 
+  rename("blancos" = `VOTOS EN BLANCO`,
+         "nulos" = `VOTOS NULOS`,
+         "codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  select(-categoria) %>% 
+  ungroup() %>% 
+  print()
+
+# Electores
+electores_sgo <- electores_gral %>% 
+  filter(categoria == "000222000000000") %>%  
+  select(-contador, -categoria) %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  ungroup() %>% 
+  print()
+
+descripcion_postulaciones %>% 
+  filter(CODIGO_CATEGORIA == "000222000000000") %>% 
+  select(CODIGO_AGRUPACION, NOMBRE_AGRUPACION)
+
+read_csv("data/listas_sen_paso2019 copy.csv") %>% 
+  filter(vot_proCodigoProvincia == "22")
+
+# Join
+sen_sgo <- electores_sgo %>% 
+  left_join(secciones) %>% 
+  left_join(votos_blancos_nulos_sgo) %>% 
+  left_join(votos_agrupaciones_sgo) %>% 
+  rename( "00095" = `22-501`,
+          "00005" = `22-502`,
+          "00009" = `22-503`,
+          "00094" = `40`,
+          "00079" = `38`,
+          "00006" = `22-179`,
+          "00061" = `22-178`,
+          "00004" = `87`,
+          "electores" = "votos") %>% 
+  select(codprov, coddepto, depto, circuito, mesa, everything()) %>% 
+  drop_na() %>% 
+  print()
+
+sen_sgo %>%  
+  select(-circuito, -mesa) %>% 
+  summarise(across(electores:`00004`, sum))
+
+sen_sgo %>% write_csv("data/definitivos/santiago_sen_paso2019.csv")
+
+# TDF ####
+# -> 000224000000000
+
+
+votos_agrupaciones_tdf <- agrupaciones %>% 
+  filter(categoria == "000224000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria, agrupacion) %>% 
+  distinct(distrito, seccion, circuito, mesa, categoria, agrupacion,.keep_all = TRUE) %>% 
+  ungroup() %>% 
+  group_by(distrito, seccion, circuito, mesa, categoria) %>% 
+  spread(agrupacion, votos) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  ungroup() %>% 
+  select(-categoria) %>% 
+  print()
+
+# Blancos Nulos
+votos_blancos_nulos_tdf <- blancos_nulos %>% 
+  filter(categoria == "000224000000000") %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  spread(contador, votos) %>% 
+  rename("blancos" = `VOTOS EN BLANCO`,
+         "nulos" = `VOTOS NULOS`,
+         "codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  select(-categoria) %>% 
+  ungroup() %>% 
+  print()
+
+# Electores
+electores_tdf <- electores_gral %>% 
+  filter(categoria == "000224000000000") %>%  
+  select(-contador, -categoria) %>% 
+  group_by(distrito, seccion, circuito, mesa) %>% 
+  mutate(mesa = str_pad(mesa, width = 6, pad = "0", side = "left"),
+         circuito = str_pad(circuito, width = 5, pad = "0", side = "left")) %>% 
+  rename("codprov" = "distrito",
+         "coddepto" = "seccion") %>% 
+  ungroup() %>% 
+  print()
+
+descripcion_postulaciones %>% 
+  filter(CODIGO_CATEGORIA == "000224000000000") %>% 
+  select(CODIGO_AGRUPACION, NOMBRE_AGRUPACION)
+
+read_csv("data/listas_sen_paso2019 copy.csv") %>% 
+  filter(vot_proCodigoProvincia == "24")
+
+# Join
+sen_tdf <- electores_tdf %>% 
+  left_join(secciones) %>% 
+  left_join(votos_blancos_nulos_tdf) %>% 
+  left_join(votos_agrupaciones_tdf) %>% 
+  rename( "00005" = `24-501`,
+          "00100" = `69`,
+          "00009" = `24-502`,
+          "00097" = `24-503`,
+          "00099" = `24-187`,
+          "00098" = `24-503`,
+          "electores" = "votos") %>% 
+  select(codprov, coddepto, depto, circuito, mesa, everything()) %>% 
+  drop_na() %>% 
+  print()
+
+sen_tdf %>%  
+  select(-circuito, -mesa) %>% 
+  summarise(across(electores:`00100`, sum))
+
+sen_tdf %>% write_csv("data/definitivos/tdf_sen_paso2019.csv")
 
